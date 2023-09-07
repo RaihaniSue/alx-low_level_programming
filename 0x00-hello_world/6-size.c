85152e1421ab019134235b6acfc6a34b4b2b7ebb
RaihaniSue <sue.raih@gmail.com>
2023-09-07 13:01:44 -0700

    size of various types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main() 
+{
+    printf("Size of char: %zu bytes\n", sizeof(char));
+    printf("Size of int: %zu bytes\n", sizeof(int));
+    printf("Size of long: %zu bytes\n", sizeof(long));
+    printf("Size of long long: %zu bytes\n", sizeof(long long));
+    printf("Size of float: %zu bytes\n", sizeof(float));
+
+    return (0);
+}
+
