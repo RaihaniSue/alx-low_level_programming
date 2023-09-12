075197b176c5a0a94bdc02a119be3bbbaa77fef8
RaihaniSue <sue.raih@gmail.com>
2023-09-12 15:51:48 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ * @n: all natural numbers from one to 98
+ * Return: the asked numbers
+ */
+void print_to_98(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			if (a != 98)
+				printf("%d, ", a);
+			else if (a == 98)
+				printf("%d\n", a);
+		}
+	} else if (n >= 98)
+	{
+		for (b = n; b >= 98; b--)
+		{
+			if (b != 98)
+				printf("%d, ", b);
+			else if (b == 98)
+				printf("%d\n", b);
+		}
+	}
+}
