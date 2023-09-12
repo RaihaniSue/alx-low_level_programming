ff534044f3573fc52788b7e7e9c8aec101c8bbfe
RaihaniSue <sue.raih@gmail.com>
2023-09-12 12:43:33 -0700

    write alphabets using a specific prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * main - Entry point
+ *
+ * Return: 0 Always (Succces)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+	return (0);
+}
