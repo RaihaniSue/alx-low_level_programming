fd929cb9eb1b96b4090d23be37aa27f57577d385
RaihaniSue <sue.raih@gmail.com>
2023-09-12 14:44:24 -0700

    sign of a number negative positive or equals to zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: the given parameter
+ * Return: 1 (positive) 0 (zero) -1 (negative)
+ */
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < '0')
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
