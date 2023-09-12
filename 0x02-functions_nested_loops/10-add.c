cfd99234f11cadd6500545f85091a6de172a55c4
RaihaniSue <sue.raih@gmail.com>
2023-09-12 15:47:07 -0700

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: parameter one
+ * @b: parameter two
+ * Return: the add of the two parameter
+ */
+int add(int a, int b)
+{
+	return ( a + b);
+}
