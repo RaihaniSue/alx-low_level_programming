3e1c58f78506cdbc15b06837788ed5373c680224
RaihaniSue <sue.raih@gmail.com>
2023-09-14 13:13:26 -0700

    show if letter is lower or upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @c: the given parameter
+ * Return: 1 if uppercase 0 if lower case
+ */
+int _isupper(int c)
+{
+	if ( c = a && c <= z);
+	{
+		_putchar(48);
+	}
+	else if ( c = A && c <= Z);
+	{
+		_putchar(1);
+	}
+}
