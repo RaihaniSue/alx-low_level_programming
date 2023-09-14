b92c085b38ae8bb6367c649d9477e5cca3fa5969
RaihaniSue <sue.raih@gmail.com>
2023-09-14 13:33:11 -0700

    test if a given character is a digit or not

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - Entry point
+ * @c: the given parameter
+ * Return: 1 if uppercase 0 if lower case
+ */
+int _isdigit(int c)
+{
+        if (c >= '0' && c <= '9')
+        {
+                return (1);
+        }
+        return (0);
+}
