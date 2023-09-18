1fdf6e4a1a64fdaf3455c0cb5acc8c9007ad0da9
RaihaniSue <sue.raih@gmail.com>
2023-09-18 13:57:43 -0700

    updating a parameter that has a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - Entry point
+ * @n: the given parameter
+ * Return: 0 Always for Success
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *p = &n;
+
+	*p = 402;
+	return (0);
+}
