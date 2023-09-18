e38d9f52d57cedcdab03d5b6bbb62324cb9b9e30
RaihaniSue <sue.raih@gmail.com>
2023-09-18 14:07:50 -0700

    swappint the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Entry point
+ * @a: first parameter
+ * @b: second parameter
+ * Return: 0 Always for success
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = c;
+}
