cd52bc4ea67b3430f84e98230ad39a8f43152598
RaihaniSue <sue.raih@gmail.com>
2023-09-28 13:39:01 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,25 @@
 #include "main.h"
 /**
- * _sqrt_recursion - Entry point
- * @n: the integer parameter
+ * look_for_root - a first entry point
+ * @i: integer parameter
+ * @j: test a root
+ * Return: 0 for Sucess -1 for failure
+ */
+int look_for_root(int i, int j)
+{
+	if (j * j > i)
+	{
+		return (-1);
+	}
+	else if (j * j == i)
+	{
+		return (j);
+	}
+	return (look_for_root(i, j + 1));
+}
+/**
+ * _sqrt_recursion - a second entry point
+ * @n: the integer
  * Return: 0 for success -1 for failure
  */
 int _sqrt_recursion(int n)
@@ -10,18 +28,5 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	int i = _sqrt_recursion(n - 1);
-
-	if (i * i <= n)
-	{
-		return (i);
-	}
-	else
-	{
-		return (i - 1);
-	}
+	return (look_for_root(n, 0));
 }
