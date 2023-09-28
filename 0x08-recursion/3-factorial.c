2dc994cbafc4865f467baab8d90f88bdce5ddfc7
RaihaniSue <sue.raih@gmail.com>
2023-09-28 12:39:26 -0700

    tou musnt be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - Entry point
+ * @n: the integer to calculate its factorial
+ * Return: 0 for success -1 for failure
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return(1);
+	}
+	else if (n < 0)
+	{
+		return(-1);
+	}
+	else
+	{
+		return(n * factorial(n - 1));
+	}
+}
