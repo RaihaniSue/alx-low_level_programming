f7d35c936ee8a05321dd3fdcce9f36116f21a9d5
RaihaniSue <sue.raih@gmail.com>
2023-09-28 13:51:29 -0700

    Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * devisor - first entry point
+ * @i: the number to bechecked
+ * @j: the devisor
+ * Return: 1 if prime, 0 for not
+ */
+int devisor(int i, int j)
+{
+	if (i == j)
+	{
+		return (1);
+	}
+	else if (i % j == 0)
+	{
+		return (0);
+	}
+	return (devisor(i, j + 1));
+}
+
+/**
+ * is_prime_number - second entry point
+ * @n: the number to be checked
+ * Return: 1 if prime 0 if not
+ */
+int is_prime_number(int n)
+{
+	return (devisor(n, j));
+}
