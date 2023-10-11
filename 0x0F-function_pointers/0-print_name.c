53b17a5c7c95c08937986ee0d0402573ad490399
RaihaniSue <sue.raih@gmail.com>
2023-10-11 14:43:53 -0700

    what is it my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_name - Entry point
+ *@name: first parameter
+ *@f: a pointer to a fun0ction that takes a characheter functrion
+ *Return: 0 Always for Success
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	if (name == NULL || f == NULL)
+		return;
+	return (fname);
+}
