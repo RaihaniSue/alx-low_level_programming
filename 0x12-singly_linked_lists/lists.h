e00704d44c0e5e01857f107fa17fb810fd435ffc
RaihaniSue <sue.raih@gmail.com>
2023-10-19 12:03:32 -0700

    signle linked lists prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_h
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+/**
+ * struct list_s - entry point
+ * @str: string
+ * @len: length
+ * @next: forward to next nide
+ *
+ * Description: single linked lists
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
