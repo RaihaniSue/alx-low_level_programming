ace9f49e3bce0461de0543823fabcdcfdb5e8309
RaihaniSue <sue.raih@gmail.com>
2023-10-19 12:08:19 -0700

    prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,20 +4,19 @@
 #include <string.h>
 #include <stdio.h>
 /**
- * struct list_s - entry point
- * @str: string
- * @len: length
- * @next: forward to next nide
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: single linked lists
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
