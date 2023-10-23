becb066ce568958e59c8fb9c71c4d50033d83185
RaihaniSue <sue.raih@gmail.com>
2023-10-23 01:20:26 -0700

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - Entry point
+ * @h: lists
+ *
+ * Return: 0 Always for success
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
