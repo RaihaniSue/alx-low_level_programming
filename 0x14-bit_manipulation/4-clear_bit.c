1924cedb25056ed47d2a6988315f1070586cffd0
RaihaniSue <sue.raih@gmail.com>
2023-10-26 01:24:06 -0700

    clearing abit at index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * clear_bit - entry point
+ * @n: long integer
+ * @index: indexing to a bit
+ *
+ * Return: 1 for success and -1 for errors
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8)
