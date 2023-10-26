bd23650092f92d3c3b2931fa5885c7a125aa67b4
RaihaniSue <sue.raih@gmail.com>
2023-10-26 01:21:54 -0700

    setting value of bit to a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * set_bit - entry point
+ * @n: long integer
+ * @index: at indexied value
+ *
+ * Return: 1 for success -1 for error
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int setbit;
