11bec5c5d296ca2ca663c907ee5f77881e1f8bd6
RaihaniSue <sue.raih@gmail.com>
2023-10-26 01:26:23 -0700

    number of bits needed to flip from one to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * flip_bits - entry point
+ * @n: the unsigned integer
+ * @m: another unsigned int
+ *
+ * Return: the num of bits needed to flip from one to another
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int a, countbit = 0;
