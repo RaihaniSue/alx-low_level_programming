9df73adb6a99f8ea169be0b0fa536ad049a612ff
RaihaniSue <sue.raih@gmail.com>
2023-10-26 01:19:33 -0700

    getting bit at index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * get_bit - entry point
+ * @n: integer long
+ * @index: indexing
+ *
+ * Return: value of bit at index -1 otherwise
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int divisor, check;
